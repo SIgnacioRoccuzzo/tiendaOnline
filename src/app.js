fa7403c0deb5c28642ebc6c86fd51397edb0117b
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-07-07 14:01:09 +0200

    saludo carola

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api', require('./routes/api'));
-
+app.get('/', (req, res) => {
+    res.send('hola carola')
+})
 
 module.exports = app;
